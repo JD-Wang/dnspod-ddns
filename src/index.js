19e4acae3565b1742f467881794eea500ca861b8
wjd <>
2023-08-24 11:21:19 +0800

    feat: 超时

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ const { SECRETID, SECRETKEY, Domain, DomainId, SubDomain, WebhookUrl } = require
 
 const DnspodClient = tencentcloud.dnspod.v20210323.Client;
 
+const timeout = 5000; // 设置超时时间为5秒
+
 // 实例化一个认证对象，入参需要传入腾讯云账户 SecretId 和 SecretKey，此处还需注意密钥对的保密
 // 代码泄露可能会导致 SecretId 和 SecretKey 泄露，并威胁账号下所有资源的安全性。以下代码示例仅供参考，建议采用更安全的方式来使用密钥，请参见：https://cloud.tencent.com/document/product/1278/85305
 // 密钥可前往官网控制台 https://console.cloud.tencent.com/cam/capi 进行获取
@@ -37,11 +39,11 @@ const client = new DnspodClient(clientConfig);
 
 async function fetchIp() {
   try {
-    const result1 = fetch("https://qifu-api.baidubce.com/ip/local/geo/v1/district")
+    const result1 = axios("https://qifu-api.baidubce.com/ip/local/geo/v1/district", { timeout })
       .then((res) => res.json())
       .then((res) => res.ip)
       .catch((e) => console.log(e));
-    const result2 = fetch("https://www.taobao.com/help/getip.php")
+    const result2 = axios("https://www.taobao.com/help/getip.php", { timeout })
       .then((res) => res.text())
       .then((res) => {
         const regex = /[0-9.]+/;
